f369d76027762d3a409906ed9806d15209afd6d6
Liu1700 <liu170045@gmail.com>
2015-06-07 16:48:13 +0800

    update

--- app/controllers/feed.go
+++ app/controllers/feed.go
@@ -17,6 +17,7 @@ func (f Feed) GetFeeds(lon float64, lat float64, distance float64, timestamp int
   respFeeds := []models.Feed{}
 
   feeds := models.GetFeeds()
+  revel.INFO.Println(feeds)
 
   userPosition := geo.NewPoint(lat, lon)
   for _, f := range feeds {
